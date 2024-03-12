5713d90744dff45187fa70a1d9e8d023c2d311e1
udayveer-singh <udayveersingh.tec@gmail.com>
2024-03-12 17:14:45 -0400

    updateshared

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,7 +1,8 @@
 def call(Map stageParams) {
+ 
     checkout([
         $class: 'GitSCM',
-        branches: [[name: stageParams.gitBranch]],
-        userRemoteConfigs: [[url: stageParams.gitUrl]]
+        branches: [[name:  stageParams.branch ]],
+        userRemoteConfigs: [[ url: stageParams.url ]]
     ])
-}
+  }
