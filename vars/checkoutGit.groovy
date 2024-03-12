c21eb188aac418e8e5b7beb0a7e0fd60f9f2904c
udayveer-singh <udayveersingh.tec@gmail.com>
2024-03-12 17:09:19 -0400

    updateshared

--- vars/checkoutGit.groovy
+++ vars/checkoutGit.groovy
@@ -1,7 +1,7 @@
 def call(String gitUrl, String gitBranch) {
     checkout([
         $class: 'GitSCM',
-        branches: [[name: gitBranch]],
-        userRemoteConfigs: [[url: gitUrl]]
+        branches: [[name: String.gitBranch]],
+        userRemoteConfigs: [[url: String.gitUrl]]
     ])
 }
